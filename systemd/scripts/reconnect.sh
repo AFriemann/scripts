dffc120bd5c805590ebbe3b6dbeea7c9f1165aed
Aljosha Friemann <aljosha@friemann.com>
2013-03-31 20:25:19 +0200

    On branch master  Initial commit   scripts repo new one

---
+++ systemd/scripts/reconnect.sh
@@ -0,0 +1,15 @@
+
+     #!/bin/sh
+     case / in
+      pre/*)
+        exit 0
+        ;;
+      post/*)
+        if ping -c 1 -W 2 www.google.de; then
+          exit 0
+        else
+          exec /usr/sbin/dhclient -v wlp3s0
+        fi
+        ;;
+    esac
+   
