b76b13d86f73f41d4a5513b88b1146a917ef7d3a
Aljosha Friemann <aljosha@friemann.com>
2013-04-24 17:51:20 +0200

    On branch master  Changes to be committed:   bug ausgemerzt durch den ExecStop aufgerufen wird obwohl   wc-reconnect.sh noch gar nicht läuft

--- systemd/scripts/wc-reconnect.sh
+++ systemd/scripts/wc-reconnect.sh
@@ -9,28 +9,28 @@ function testConn {
 }
 
 # runs when out of sleep
-case / in
-  pre/*)
-    exit 0
-    ;;
-  post/*)
-    tries=0
-    
-    while !(ping -c 1 -W 2 www.google.de); do
-      if [ tries -gt 3 ]; then
-        echo "Connection could not be established, see logs for more!"
-        exit 1
-      else
-        wconnect
-      fi
-
-      let tries=tries+1 
-    done
-
-    echo "Reconnect successful or connection already established!"
-    exit 0
-    ;;
-esac
+#case / in
+  #pre/*)
+    #exit 0
+    #;;
+  #post/*)
+    #tries=0
+    #
+    #while !(ping -c 1 -W 2 www.google.de); do
+      #if [ tries -gt 3 ]; then
+        #echo "Connection could not be established, see logs for more!"
+        #exit 1
+      #else
+        #wconnect
+      #fi
+#
+      #let tries=tries+1 
+    #done
+#
+    #echo "Reconnect successful or connection already established!"
+    #exit 0
+    #;;
+#esac
 
 #runs otherwise
 testConn &
