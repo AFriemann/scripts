467410435219e06b20eda78db6391a41bf3fc972
Aljosha Friemann <aljosha@friemann.com>
2013-04-24 03:31:27 +0200

    On branch master  Changes to be committed:   awesome version

--- systemd/scripts/reconnect.sh
+++ systemd/scripts/reconnect.sh
@@ -8,7 +8,7 @@
           if ping -c 1 -W 2 www.google.de; then
             exit 0
           else
-            exec /usr/sbin/dhclient -v wlp3s0
+            exec systemctl restart dhclient@wlp3s0.service
           fi
           ;;
       esac
