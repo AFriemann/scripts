3b69dfda9ca5bf373c35de3d4070e64cea44d0ce
Aljosha Friemann <aljosha@friemann.com>
2013-04-29 14:41:41 +0200

    On branch master  Changes to be committed:   mal sehen wie sich die version jetzt bewährt   wconnect verwaltet dhclient & wpa_supplicant services   wconnect.service startet script das regelmäßig wconnect aufruft

--- systemd/scripts/wc-reconnect.sh
+++ systemd/scripts/wc-reconnect.sh
@@ -1,36 +1,22 @@
 #!/bin/sh
 
-function testConn {
-  while true; do
-    echo "Hi, I'm your wconnect-reconnect script and I'm still running!"
-    wconnect
-    sleep 10
-  done
-}
-
 # runs when out of sleep
-#case / in
-  #pre/*)
-    #exit 0
-    #;;
-  #post/*)
-    #tries=0
-    #
-    #while !(ping -c 1 -W 2 www.google.de); do
-      #if [ tries -gt 3 ]; then
-        #echo "Connection could not be established, see logs for more!"
-        #exit 1
-      #else
-        #wconnect
-      #fi
-#
-      #let tries=tries+1 
-    #done
-#
-    #echo "Reconnect successful or connection already established!"
-    #exit 0
-    #;;
-#esac
+case / in
+  pre/*)
+    exit 0
+    ;;
+  post/*)
+    tries=0
+    
+    while !(wconnect); do
+      if [ tries -gt 3 ]; then
+        echo "Connection could not be established, see logs for more!"
+        exit 1
+      fi
+      let tries=tries+1 
+    done
 
-#runs otherwise
-testConn &
+    echo "Reconnect successful or connection already established!"
+    exit 0
+    ;;
+esac
