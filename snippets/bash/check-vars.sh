0fcadce03ba1d8360e1f93507a85ad4ea8aef96b
Aljosha Friemann <aljosha.friemann@gmail.com>
2017-12-28 22:17:26 +0100

    check-vars bash snippet

---
+++ snippets/bash/check-vars.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# -*- coding: utf-8 -*-
+# check-vars.sh
+# created: 2017-12-28
+# author: Aljosha Friemann a.friemann@automate.wtf
+
+: ${HOSTNAME?} ${USER?} ${MAIL?} ${FOOBAR?}
+
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4 fenc=utf-8
