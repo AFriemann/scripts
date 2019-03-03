8809e3fc02921c2e3197e59e986035982b2642c9
Aljosha Friemann <a.friemann@automate.wtf>
2019-03-03 17:44:28 +0100

    removed license headers and linting

--- bash/chpgsql.sh
+++ bash/chpgsql.sh
@@ -1,14 +1,4 @@
 #! /bin/bash
-#
-# chpgsql.sh
-# Copyright (C) 2019 Aljosha Friemann <a.friemann@automate.wtf>
-#
-# Distributed under terms of the 3-clause BSD license.
-#
-# Disclaimer:
-#
-#   completely ripped off from chruby scripts
-#
 
 set -E -o pipefail
 IFS=$'\n\t'
@@ -32,35 +22,37 @@ chpgsql_use() {
 chpgsql_reset() {
     [[ -z "$PGSQL_ROOT" ]] && return
 
-    PATH=":$PATH:"; PATH="${PATH//:$PGSQL_ROOT\/bin:/:}"
+    PATH=":$PATH:"
+    PATH="${PATH//:$PGSQL_ROOT\/bin:/:}"
 
-    PATH="${PATH#:}"; PATH="${PATH%:}"
+    PATH="${PATH#:}"
+    PATH="${PATH%:}"
     unset PGSQL_ROOT PGSQL_VERSION
     hash -r
 }
 
 chpgsql() {
     case "$1" in
-        system) chpgsql_reset ;;
-        *)
-            local dir match
+    system) chpgsql_reset ;;
+    *)
+        local dir match
 
-            for dir in "${POSTIES[@]}"; do
-                dir="${dir%%/}"
-                case "${dir##*/}" in
-                    "$1") match="$dir" && break ;;
-                    *"$1"*) match="$dir" ;;
-                esac
-            done
+        for dir in "${POSTIES[@]}"; do
+            dir="${dir%%/}"
+            case "${dir##*/}" in
+            "$1") match="$dir" && break ;;
+            *"$1"*) match="$dir" ;;
+            esac
+        done
 
-            if [[ -z "$match" ]]; then
-                echo "chpgsql: unknown postgresql: $1" >&2
-                return 1
-            fi
+        if [[ -z "$match" ]]; then
+            echo "chpgsql: unknown postgresql: $1" >&2
+            return 1
+        fi
 
-            shift
-            chpgsql_use "$match" "$*"
-            ;;
+        shift
+        chpgsql_use "$match" "$*"
+        ;;
     esac
 }
 
@@ -71,7 +63,8 @@ chpgsql_auto() {
         dir="${dir%/*}"
 
         if { read -r version <"$dir/.pgsql-version"; } 2>/dev/null || [[ -n "$version" ]]; then
-            if [[ "$version" == "$PGSQL_AUTO_VERSION" ]]; then return
+            if [[ "$version" == "$PGSQL_AUTO_VERSION" ]]; then
+                return
             else
                 PGSQL_AUTO_VERSION="$version"
                 chpgsql "$version"
