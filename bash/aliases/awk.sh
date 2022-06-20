23da08a6f41e24c59ce8f604a0c3a989789ba540
Aljosha Friemann <aljosha.friemann@gmail.com>
2022-06-20 14:40:08 +0200

    meh

--- bash/aliases/awk.sh
+++ bash/aliases/awk.sh
@@ -1,3 +1,2 @@
 alias hours="awk -F',' 'OFS=\",\" { \$4 = ((\$3 * 60 - \$2 * 60) + (substr(\$3, 4) - substr(\$2, 4))) / 60 } 1'"
 alias total_hours="hours | awk -F',' '{ print \$4 }' | paste -sd+ - | bc"
-
